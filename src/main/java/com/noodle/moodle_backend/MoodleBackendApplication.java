99d6dae2277372faf24ee4145e04f106f92751ab
Noodle-bg <142234652+Noodle-bg@users.noreply.github.com>
2026-04-07 22:14:26 +0530

    initial spring boot scaffold

---
+++ src/main/java/com/noodle/moodle_backend/MoodleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.noodle.moodle_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodleBackendApplication.class, args);
+	}
+
+}
